7a0029a41734da1e4f775b5233d383d4849e6ec8
Tomiloba21 <olowo15tomiloba@gmail.com>
2024-03-18 21:56:49 +0100

    added some properties to the Customer class

--- src/java/AllInOneApp/Customer.java
+++ src/java/AllInOneApp/Customer.java
@@ -4,10 +4,212 @@
  */
 package AllInOneApp;
 
+import org.springframework.stereotype.Component;
+
 /**
  *
  * @author admin
  */
+
+@Component
 public class Customer {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String address;
+    private String checkInDate;
+    private String checkInMonth;
+    private String checkInYear;
+    private String emailID;
+    private String gender;
+    private String City;
+    private String phone;
+    private String userName;
+    private String password;
+
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the middleName
+     */
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    /**
+     * @param middleName the middleName to set
+     */
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the checkInDate
+     */
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    /**
+     * @param checkInDate the checkInDate to set
+     */
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    /**
+     * @return the checkInonth
+     */
+    public String getCheckInMonth() {
+        return checkInMonth;
+    }
+
+    /**
+     * @param checkInonth the checkInonth to set
+     */
+    public void setCheckInMonth(String checkInonth) {
+        this.checkInMonth = checkInonth;
+    }
+
+    /**
+     * @return the checkInYear
+     */
+    public String getCheckInYear() {
+        return checkInYear;
+    }
+
+    /**
+     * @param checkInYear the checkInYear to set
+     */
+    public void setCheckInYear(String checkInYear) {
+        this.checkInYear = checkInYear;
+    }
+
+    /**
+     * @return the emailID
+     */
+    public String getEmailID() {
+        return emailID;
+    }
+
+    /**
+     * @param emailID the emailID to set
+     */
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the City
+     */
+    public String getCity() {
+        return City;
+    }
+
+    /**
+     * @param City the City to set
+     */
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
+
